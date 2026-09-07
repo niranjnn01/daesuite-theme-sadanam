ed7f451bec742b66b7bdadb63513e91cd9b4c155
Rakesh <niranjnn01@gmail.com>
2026-09-07 22:53:20 +0530

    header client name comes from  now

--- views/components/branding.blade.php
+++ views/components/branding.blade.php
@@ -2,7 +2,7 @@
                  
 <div class="flex justify-center gap-4 items-center">
     <a href="/" class="flex items-center">
-        <x-image src="{{tenant_asset('images/logo.svg')}}" alt="Logo" class="h-12 w-auto" />
+        <x-image src="{{tenant_asset('images/logo.svg')}}" alt="Logo" class="h-30 w-auto" />
     </a>
-    <span class="text-xl">{{ config('client.name') }}</span>
+    <span class="text-xl">{{ $organization->name }}</span>
 </div>
